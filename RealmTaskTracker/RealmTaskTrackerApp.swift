fd12a1991976dfadbe56c8edda37e487ebde5342
Ben Chatelain <ben@octop.ad>
2020-09-15 18:28:50 -0600

    💀 Create single view app

---
+++ RealmTaskTracker/RealmTaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmTaskTrackerApp.swift
+//  RealmTaskTracker
+//
+//  Created by Ben Chatelain on 9/15/20.
+//
+
+import SwiftUI
+
+@main
+struct RealmTaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
