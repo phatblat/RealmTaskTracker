97eaa361a162c87cf80bc78e1b704cad9ba6d7aa
Ben Chatelain <ben@octop.ad>
2020-09-15 19:50:52 -0600

    ✨ Add Constants

---
+++ RealmTaskTracker/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  RealmTaskTracker
+//
+//  Created by Ben Chatelain on 9/15/20.
+//
+
+import RealmSwift
+
+struct Constants {
+    static let REALM_APP_ID = "task-tracker-seidr"
+}
+
+let app = App(id: Constants.REALM_APP_ID)
