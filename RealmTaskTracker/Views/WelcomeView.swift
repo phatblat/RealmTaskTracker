aa72029962e12b2ca2b934368eba4fa87d065041
Ben Chatelain <ben@octop.ad>
2020-09-15 21:25:22 -0600

    ✨ Implement signUp

--- RealmTaskTracker/Views/WelcomeView.swift
+++ RealmTaskTracker/Views/WelcomeView.swift
@@ -5,12 +5,14 @@
 //  Created by Ben Chatelain on 9/15/20.
 //
 
+import RealmSwift
 import SwiftUI
 
 struct WelcomeView: View {
     @State private var loading = false
     @State private var username: String = ""
     @State private var password: String = ""
+    @State private var message: String = ""
 
     var body: some View {
         LoadingView(isShowing: $loading) {
@@ -21,17 +23,37 @@ struct WelcomeView: View {
                     TextField("Username", text: $username)
                     SecureField("Password", text: $password)
                     Button("Sign In", action: signIn)
+                    Button("Sign Up", action: signUp)
                 }
-                Button("Sign Up", action: signUp)
+                Text(message)
             }
         }
     }
 
-    func signIn() {
+    func signUp() {
         loading.toggle()
+
+        let emailPassAuth = app.emailPasswordAuth()
+        emailPassAuth.registerEmail(username, password: password, completion: { (error: Error?) in
+            DispatchQueue.main.sync {
+                loading.toggle()
+
+                guard error == nil else {
+                     print("Signup failed: \(error!)")
+                     message = "Signup failed: \(error!.localizedDescription)"
+                     return
+                }
+                print("Signup successful!")
+
+                // Registering just registers. Now we need to sign in,
+                // but we can reuse the existing username and password.
+                message = "Signup successful! Signing in..."
+                signIn()
+            }
+        })
     }
 
-    func signUp() {}
+    func signIn() {}
 }
 
 struct WelcomeView_Previews: PreviewProvider {
