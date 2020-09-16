80d38d542086f5e01bb402d108f65b2f6a9dcdaa
Ben Chatelain <ben@octop.ad>
2020-09-16 15:22:49 -0600

    💥 Change User._id type to ObjectId

--- RealmTaskTracker/Models.swift
+++ RealmTaskTracker/Models.swift
@@ -12,12 +12,11 @@ typealias ProjectId = String
 
 class Project: Object {
     @objc dynamic var _id: ObjectId = ObjectId.generate()
-    @objc dynamic var _partition: String? = nil
+    @objc dynamic var _partition: ProjectId = ""
     @objc dynamic var name: String = ""
     override static func primaryKey() -> String? {
         return "_id"
     }
-
     convenience init(partition: String, name: String) {
         self.init()
         self._partition = partition
@@ -26,10 +25,11 @@ class Project: Object {
 }
 
 class User: Object {
-    @objc dynamic var _id: String = ""
-    @objc dynamic var _partition: String? = nil
+    @objc dynamic var _id: ObjectId = ObjectId.generate()
+    @objc dynamic var _partition: ProjectId = ""
     @objc dynamic var image: String? = nil
     @objc dynamic var name: String = ""
+    @objc dynamic var user_id: String = ""
     override static func primaryKey() -> String? {
         return "_id"
     }
@@ -43,9 +43,9 @@ case Complete
 
 class Task: Object {
     @objc dynamic var _id: ObjectId = ObjectId.generate()
-    @objc dynamic var _partition: ProjectId? = nil
+    @objc dynamic var _partition: ProjectId = ""
     @objc dynamic var assignee: User?
-    @objc dynamic var name = ""
+    @objc dynamic var name: String = ""
     @objc dynamic var status = TaskStatus.Open.rawValue
 
     var statusEnum: TaskStatus {
@@ -56,11 +56,9 @@ class Task: Object {
             status = newValue.rawValue
         }
     }
-
     override static func primaryKey() -> String? {
         return "_id"
     }
-
     convenience init(partition: String, name: String) {
         self.init()
         self._partition = partition
