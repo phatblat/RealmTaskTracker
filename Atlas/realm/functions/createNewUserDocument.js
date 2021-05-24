916e735bd0ed2acb4a316c3e2e30164499e862b4
Ben Chatelain <ben@octop.ad>
2021-05-24 16:18:56 -0600

    🐛 Fix createNewUserDocument realm sync function

--- Atlas/realm/functions/createNewUserDocument.js
+++ Atlas/realm/functions/createNewUserDocument.js
@@ -2,7 +2,7 @@ exports = async function createNewUserDocument({ user }) {
   const cluster = context.services.get("mongodb-atlas");
   const users = cluster.db("tracker").collection("users");
   return await users.insertOne({
-    _partition: "My Project",
+    _partition: `user=${user.id}`,
     user_id: user.id,
     name: user.data.email,
   });
